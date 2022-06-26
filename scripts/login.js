37270b7d0851a364943d5873659b41ecf0f7c57d
AntonioNigro1 <46200602+AntonioNigro1@users.noreply.github.com>
2022-06-26 11:30:13 -0300

    Update login.js
    Atualizado os fetchs para fazer requisições para o heroku

--- scripts/login.js
+++ scripts/login.js
@@ -1,76 +1,80 @@
-document.getElementById('signin').addEventListener('click', function(){
+document.getElementById('signin').addEventListener('click', function () {
     document.getElementById('signupbox').classList.toggle('displaynone', false);
 })
 
-document.getElementById('signup').addEventListener('click', function(){
+document.getElementById('signup').addEventListener('click', function () {
 
     document.getElementById('signupbox').classList.toggle('displaynone', false);
 })
 
-function verificaEmail(emailinput){
+function verificaEmail(emailinput) {
     console.log(emailinput.value.length);
-    if(emailinput.value.indexOf("@") == -1 || emailinput.value.indexOf("@")==0 || 
-    emailinput.value.indexOf("@")==emailinput.value.length-1 || emailinput.value.length <3){ 
+    if (emailinput.value.indexOf("@") == -1 || emailinput.value.indexOf("@") == 0 ||
+        emailinput.value.indexOf("@") == emailinput.value.length - 1 || emailinput.value.length < 3) {
         document.getElementById('erro').innerHTML = "Erro, email invalido";
         document.getElementById('erro').classList.toggle('displaynone', false);
         return false;
-    }else{
+    } else {
         return true;
     }
-
 }
-function verificaSenha(pswinput){
+
+function verificaSenha(pswinput) {
     senha = pswinput.value;
 
-    if(senha.length >= 3) return true; 
-    else{ 
+    if (senha.length >= 3) return true;
+    else {
         document.getElementById('erro').innerHTML = "Erro, senha invalido";
         document.getElementById('erro').classList.toggle('displaynone', false);
         return false;
     }
 }
-//para testar o registro(signup) temos de usar o usuario definido pela API (para testes)
-//podendo escolher qualquer senha para o registro
-//email: eve.holt@reqres.in
 
-document.getElementById('signup2').addEventListener('click', async function(emailinput, pswinput){
-    emailinput=document.getElementById('user_email');
-    pswinput=document.getElementById('user_password');
+document.getElementById('signup2').addEventListener('click',  async function (nomeinput, emailinput, pswinput) {
+    nomeinput = document.getElementById('user_name');
+    emailinput = document.getElementById('user_email');
+    pswinput = document.getElementById('user_password');
 
-    if(verificaEmail(emailinput) && verificaSenha(pswinput)){
-        const json = await fetch('https://reqres.in/api/register',{
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body:JSON.stringify({email:emailinput.value,
-            password:pswinput.value})
-            }
-        )
-        if(!json.ok){
-        document.getElementById('erro').innerHTML = "Erro na consulta a API";
-        document.getElementById('erro').classList.toggle('displaynone', false);
+    if (verificaEmail(emailinput) && verificaSenha(pswinput)) {
+        const json = await fetch('https://web-api-dicionario-simples.herokuapp.com/Users/signup', {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' },
+            mode: 'cors',
+            body: JSON.stringify({
+                nome: nomeinput.value,
+                email: emailinput.value,
+                senha: pswinput.value,
+                admin: false
+            })
+        });
+        if (!json.ok) {
+            document.getElementById('erro').innerHTML = "Erro na consulta a API";
+        } else {
+            document.getElementById('erro').innerHTML = "Usuario cadastrado com sucesso";
         }
+        document.getElementById('erro').classList.toggle('displaynone', false);
     }
 
 })
 
-//para testar o login podemos utilizar o registrado por nos 
-//(utilizando o email padrão mais a senha de escolha)
-document.getElementById('signin2').addEventListener('click', async function(emailinput, pswinput){
-    emailinput=document.getElementById('user_email');
-    pswinput=document.getElementById('user_password');
-    if(verificaEmail(emailinput) && verificaSenha(pswinput)){
-        const json = await fetch('https://reqres.in/api/login',{
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body:JSON.stringify({email:emailinput.value,
-            password:pswinput.value})
-            }
-        )
+document.getElementById('signin2').addEventListener('click', async function (emailinput, pswinput) {
+    emailinput = document.getElementById('user_email');
+    pswinput = document.getElementById('user_password');
+    if (verificaEmail(emailinput) && verificaSenha(pswinput)) {
+        const json = await fetch('https://web-api-dicionario-simples.herokuapp.com/Users/signin', {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                email: emailinput.value,
+                senha: pswinput.value
+            })
+        });
         let data = await json.json();
-        if(json.status == 200) localStorage.setItem('token', JSON.stringify(data.token));
-        else{
-        document.getElementById('erro').innerHTML = "Erro na consulta a API";
-        document.getElementById('erro').classList.toggle('displaynone', false);
+        if (json.status == 200) {
+            localStorage.setItem('token', data.token);
+        } else {
+            document.getElementById('erro').innerHTML = "Erro na consulta a API";
+            document.getElementById('erro').classList.toggle('displaynone', false);
         }
         logado();
     }
